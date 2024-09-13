c41aa5640cf74955109dbaba94c8b274f59ff7dd
Monica Turner <turnermon@gmail.com>
2024-09-13 16:44:07 +0200

    HDXDSYS-845 add test files

--- src/hdx/scraper/dtm/__main__.py
+++ src/hdx/scraper/dtm/__main__.py
@@ -86,8 +86,6 @@ def main(
 if __name__ == "__main__":
     facade(
         main,
-        # hdx_site="dev",
-        hdx_site="stage",
         user_agent_config_yaml=join(expanduser("~"), ".useragents.yaml"),
         user_agent_lookup=_USER_AGENT_LOOKUP,
         project_config_yaml=join(
